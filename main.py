5b6fa4da71525758a77438e8fad709791f78613b
Muhammad Faridan Sutariya <88027268+MuhFaridanSutariya@users.noreply.github.com>
2024-11-14 22:31:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
+
 if __name__ == '__main__':
   app.run(port=5000)
